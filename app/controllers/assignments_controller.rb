ea25ac73d1a1cbce515c69505b64b12f3ed47b3d
Matthew Flood <mflood@shutterstock.com>
2014-09-30 11:18:18 -0600

    added url to assingment

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -69,6 +69,6 @@ class AssignmentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def assignment_params
-      params.require(:assignment).permit(:course_id, :name, :finished, :due_date, :notes)
+      params.require(:assignment).permit(:course_id, :name, :finished, :due_date, :notes, :url)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140930144204) do
+ActiveRecord::Schema.define(version: 20140930171252) do
 
   create_table "assignments", force: true do |t|
     t.integer  "course_id"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140930144204) do
     t.text     "notes"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "url"
   end
 
   add_index "assignments", ["course_id"], name: "index_assignments_on_course_id"
