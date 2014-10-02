cb0c7cc0d43d0a8a98570202fb307160bf046728
Matthew Flood <mflood@shutterstock.com>
2014-10-02 09:55:40 -0600

    Fixed model relationship

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ActiveRecord::Base
     validates_presence_of :name
     belongs_to :school
-    has_many :classes, dependent: :restrict_with_error
+    has_many :assignments, dependent: :restrict_with_error
 end
